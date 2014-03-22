4a4ff78fe3f2f9c4cb406bf3164cdd6d8a73fb39
Matthias Vogelgesang <matthias.vogelgesang@gmail.com>
2014-03-22 14:53:00 +0100

    Add a package.json

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup, find_packages
+import json
+
+def read_from_package():
+    with open('package.json', 'r') as f:
+        pkg = json.load(f)
+        return dict(version=pkg['version'],
+                    description=pkg['description'])
 
 setup(
     name='clibdoc',
-    version='0.1.0',
     packages=find_packages(),
+    install_requires=['jinja2'],
     data_files=[('clibdoc/data', ['data/Doxyfile.in',
                                   'data/index.html',
                                   'data/main.html',
                                   'data/clibdoc.css'])],
-    scripts=['bin/clib-doc']
+    scripts=['bin/clib-doc'],
+    **read_from_package()
 )
